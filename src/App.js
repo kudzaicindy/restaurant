40c13afa6cd86b9246b7bcdea6208a9e67165003
kudzaicindy <kudzaicpemhiwa@gmail.com>
2024-09-30 08:54:55 +0200

    Fix build configuration and add necessary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import Menu from './components/Menu';
-import About from './components/About';
-import Blog from './components/Blog';
-import Reservations from './components/Reservations';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/reservations" element={<Reservations />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <h1>Welcome to our Restaurant</h1>
+    </div>
   );
 }
 
