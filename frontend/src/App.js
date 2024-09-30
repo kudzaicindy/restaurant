705006b68986ede7f2d6e315b29abd61643645d9
kudzaicindy <kudzaicpemhiwa@gmail.com>
2024-09-30 09:17:13 +0200

    Add missing frontend files and update structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReservationForm from './components/ReservationForm';
-import MenuList from './components/MenuList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Our Restaurant</h1>
-      <MenuList />
-      <h2>Make a Reservation</h2>
-      <ReservationForm />
+      <h1>Welcome to our Restaurant</h1>
     </div>
   );
 }
